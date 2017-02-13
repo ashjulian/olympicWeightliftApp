fd1b629e34b27d4065ca2f95c50e5ff2e7433ada
penCoding <CMI@HAL-Mother-Base.local>
2017-02-13 18:45:11 -0400

    Button Images Linked
    Figured out how to allow the clients to access the resource directories
    to link in the images for the buttons.

--- node.js
+++ node.js
@@ -10,6 +10,7 @@ var server = require('http').Server(app);
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/judge/judge.htm');
 });
+app.use(express.static(__dirname + '/judge'));
 // app.get('/', function(req, res){
 //     res.sendFile(__dirname + '/judge/index.htm');
 // });
@@ -21,15 +22,28 @@ console.log("Sever Started...");
 console.log("Listening on port 8080");
 
 var SOCKET_LIST = [];
+var VOTE_LIST = [];
 
 // this block of code runs when a client connects
 var io = require('socket.io')(server, {});
 io.sockets.on('connection', function(socket){
 
 // temporary form of Id'ing the client. we will eventually do a login system.
-socket.id = Math.random();
+socket.id = Math.floor(10*Math.random());
 
 socket.on('vote', function(data){
+    VOTE_LIST.push({
+            judge:socket.id,
+            vote:data.vote,   
+        });
+    for(var i in VOTE_LIST){
+        var vote = VOTE_LIST[i];
+        if(data.vote === vote.vote){
+            //console.log('Decision Made');
+            VOTE_LIST.length = 0;
+        }
+    }
+
     // when vote is heard prints judges vote to consol for now
     console.log('Judge ' + socket.id + " voted " + data.vote);
     //emit the vote back to all connectec sockets
