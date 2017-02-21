d93912f3a1093f2d382ebd9335e23a4aff1eff21
penCoding <CMI@HAL-Mother-Base.local>
2017-02-21 14:27:34 -0400

    Login
    Hard coded a login system. No db.

--- node.js
+++ node.js
@@ -8,7 +8,7 @@ var app = express();
 var server = require('http').Server(app);
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/judge/index.htm');
+    res.sendFile(__dirname + '/judge/testIndex.htm');
 });
 app.use(express.static(__dirname + '/judge'));
 // app.get('/', function(req, res){
@@ -21,7 +21,44 @@ server.listen(8080);
 console.log("Sever Started...");
 console.log("Listening on port 8080");
 
+var Judge = function(id){
+    var self = {
+        id: id,
+        vote: ""
+    }
+
+}
+
+// ----------------------------------------------------------------------- //
+// --------------------------- SIGN IN METHODS --------------------------- //
+// ----------------------------------------------------------------------- //
+
+var USERS = {
+    // username:password
+    "judge1":"123",
+    "judge2":"456",
+    "judge3":"789",
+    "coordinator":"1010",
+}
+
+var isValidPassword = function(data){
+    return USERS[data.username] === data.password; 
+}
+
+var isUsernameTaken = function(data){
+    return USERS[data.username]; 
+}
+
+var addUser = function(data){
+    USERS[data.username] = data.password; 
+}
+
+// ----------------------------------------------------------------------- //
+// ----------------------------------------------------------------------- //
+// ----------------------------------------------------------------------- //
+
 var SOCKET_LIST = [];
+var JUDGE_LIST = [];
 var VOTE_LIST = [];
 
 // this block of code runs when a client connects
@@ -31,16 +68,42 @@ io.sockets.on('connection', function(socket){
 // temporary form of Id'ing the client. we will eventually do a login system.
 socket.id = Math.floor(10*Math.random());
 
+socket.on('signIn', function(data){
+            if(isValidPassword(data)){
+                socket.emit('signInResponse', {success:true});
+            }else{
+                socket.emit('signInResponse', {success:false});
+            }
+    });
+
+    socket.on('signUp', function(data){
+        if((data.username !== '') && (data.password !== '')){
+            if(isUsernameTaken(data)){
+                socket.emit('signUpResponse', {success:false});
+            }else{
+                addUser(data);
+                socket.emit('signUpResponse', {success:true});
+            }
+        }else{
+            socket.emit('signUpResponse', {success:false});
+        }
+    });
+
+
+
+
 socket.on('vote', function(data){
-    VOTE_LIST.push({
-            judge:socket.id,
-            vote:data.vote,   
-        });
+    
     for(var i in VOTE_LIST){
         var vote = VOTE_LIST[i];
         if(data.vote === vote.vote){
-            //console.log('Decision Made');
+            console.log('Decision Made');
             VOTE_LIST.length = 0;
+        }else{
+            VOTE_LIST.push({
+            judge:socket.id,
+            vote:data.vote,   
+            });
         }
     }
 
